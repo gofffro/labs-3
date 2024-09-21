22865545c1432658898a0b5daa4716bf5a592736
gofffro <vazheninstepan@gmail.com>
2024-09-21 20:28:56 +0300

    Uploud main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  setlocale(LC_ALL, "Russian");
+  double P;
+  const double e = 115.0;
+  const double r = 29.7;
+  int R = 10;
+  
+  while (R <= 300) {
+    P = R * (pow((e / (R + r)), 2));
+
+    cout << fixed << setprecision(3) << "R: " << R << " Ом ";
+    cout << fixed << setprecision(3) << " P: " << P << " Вт " << endl;
+
+    if (R < 50) {
+      R = R + 10;
+    } else {
+      R = R + 50;
+    }
+  }
+}

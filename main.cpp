70e262cefab6b4e71bcd2882e1dea2dd7995d784
gofffro <vazheninstepan@gmail.com>
2024-09-21 20:33:22 +0300

    Update main.cpp
    Добавлен цикл с постусловием

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main() {
   const double e = 115.0;
   const double r = 29.7;
   int R = 10;
-  // Предусловия
+  
+  // Предусловие
   cout << "Цикл с предусловием: " << endl;
   while (R <= 300) {
     P = R * (pow((e / (R + r)), 2));
@@ -24,4 +25,20 @@ int main() {
       R = R + 50;
     }
   }
+  
+  R = 10;
+  cout << "Цикл с постусловием: " << endl;
+  // Постусловие
+  do {
+    P = R * (pow((e / (R + r)), 2));
+
+    cout << fixed << setprecision(3) << "R: " << R << " Ом ";
+    cout << fixed << setprecision(3) << " P: " << P << " Вт " << endl;
+
+    if (R < 50 & R >= 10) {
+      R = R + 10;
+    } else {
+      R = R + 50;
+    }
+  } while (R <= 300);
 }
